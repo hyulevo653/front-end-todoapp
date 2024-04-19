d480ca744a1eff9f62980b72978c37fbb051c321
hyulevo653 <duchuy653201@gmail.com>
2024-04-19 21:19:16 +0700

    code frontend

---
+++ app/components/Appbar.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { HomeModernIcon } from '@heroicons/react/16/solid'
+import { Navbar, NavbarContent, NavbarMenuToggle, NavbarBrand, NavbarItem, Button, NavbarMenu, NavbarMenuItem } from '@nextui-org/react'
+import Link from 'next/link'
+import React, { ReactNode } from 'react'
+
+interface Props {
+  children : ReactNode;
+}
+
+
+const Appbar = ({children} : Props) => {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  return (
+    <Navbar className='shadow-md' onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+            <Link href={"/"} className='flex items-center text:primary-400 hover:scale-110 hover:primary-500 transition-colors'>
+            <HomeModernIcon className='w-16'/>
+          <p className="font-bold text-inherit">TODO APP</p>
+            </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+    
+      </NavbarContent>
+      <NavbarContent justify="end">
+       {children}
+      </NavbarContent>
+      <NavbarMenu>
+      </NavbarMenu>
+    </Navbar>
+  )
+}
+
+export default Appbar
